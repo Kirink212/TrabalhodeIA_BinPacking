7a5a0fbe69962457ecd0f8c66492a83fccb34353
Luís Fernando Teixeira Bicalho <lfernandotexbicalho@hotmail.com>
2018-05-06 21:20:24 -0300

    Adding comments

--- main.py
+++ main.py
@@ -74,57 +74,71 @@ def min_item(box):
 #First Fit Algorithm: Receives a list of items in descending order,
 #inserting the item on the first bin with enough space to store it
 def first_fit_algorithm_modified(bin_capacity, items_list, show_log):
-	#Bin list where each bin is represented as a string of 0's and 1's, where each position that have number 1 represents which item is on the bin 
+	# Bin list where each bin is represented 
+	# as a string of 0's and 1's, 
+	# where each position that have number 1 
+	# represents which item is on the bin 
 	bins_list = []
 	bin_index = 0
 	item_index = 0
-	string_bin_pattern = "0"*len(items_list) #Creates a string with size equals the number of items in the problem, filled with zeroes
+	# Creates a string with size equals the number of items 
+	# in the problem, filled with zeroes
+	string_bin_pattern = "0"*len(items_list)
 
+	# Start counting algorithm runtime
 	t0 = time()
 	
-	#Repeats until items_list is over
+	# Repeats until items_list is over
 	while item_index < len(items_list): 
 	
-		#If the list is empty, create the first Bin filled with 0's.
+		# If the list is empty, create the first Bin filled with 0's.
 		if bins_list == []:
 			bins_list.append(string_bin_pattern)
 
-		#Returns two values. The first if the total weight of the bin and the second is ignored.
+		# Returns two values. The first if the total weight
+		# of the bin and the second is ignored.
 		bin_sum, aux = item_size(bins_list[bin_index])
 		
-		#Variable that receives the item weight and sums with the bin weight 
+		# Variable that receives the item weight
+		# and sums with the bin weight 
 		check_sum = items_list[item_index] + bin_sum
 		
-		#If the sum of item weight and bin weight surpass the bin capcity, we go to the next bin.
+		# If the sum of item weight and bin weight
+		# surpass the bin capcity, we go to the next bin.
 		if check_sum > bin_capacity:
 
-			#If there is no other bins, we create one.
+			# If there is no other bins, we create one.
 			if bin_index == len(bins_list) - 1:
 				bins_list.append(string_bin_pattern)
 				
-			#Go to the next bin
+			# Go to the next bin
 			bin_index = bin_index + 1
 			
-		#But if we can put the item inside the bin
+		# But if we can put the item inside the bin
 		else:
 			
-			#As strings in python are immutable, we convert it to a list
+			# As strings in python are immutable, 
+			# we convert it to a list
 			aux_list = list(bins_list[bin_index])
 			
-			#We change the 0 value to 1, indicating that the item is in the bin
+			# We change the 0 value to 1, 
+			# indicating that the item is in the bin
 			aux_list[item_index] = '1'
 			
-			#Converts the list to a string and replaces the old string with the new one.
+			# Converts the list to a string and replaces 
+			# the old string with the new one.
 			bins_list[bin_index] = "".join(aux_list)
 
-			#Go to the next item
+			# Go to the next item
 			item_index = item_index + 1
 			
-			#Go back to the initial bin
+			# Go back to the initial bin
 			bin_index = 0
 
+	# Finished to count algorithm runtime
 	t1 = time()
 
+	# Show log just if show_log is True
 	if show_log:
 		print("-------------------------------")
 		print("Total de bins: ", len(bins_list))
@@ -140,31 +154,44 @@ def first_fit_algorithm_modified(bin_capacity, items_list, show_log):
 
 	return bins_list
 
-#Verifies if the bin is empty or not
+# Verifies if the bin is empty or not
 def empty_bin(bin, items_list):
 	return (bin == "0"*len(items_list))
 
-#Initially uses the first fit method to create an initial state and from it,
+# Initially uses the first fit method to create an initial state and from it,
 # make swaps and reallocates to arrive at better solutions.
-#If from the initial state the algorithm can't find a better state, 
-#it randomizes the items list and starts again. But only do it a fixed amount of times.
-
+# If from the initial state the algorithm can't find a better state, 
+# it randomizes the items list and starts again. But only do it a fixed 
+# amount of times.
 def hill_climbing_test(bin_capacity, items_list, restart_limit, total_iterations, total_no_better):
-	t0 = time() #Starts to count the execution time
-	restart_algorithm = False #Verifies if you did a restart
-	restart_counter = 0 #Count the number of restarts
-	bins_list_solution = [] #List of solutions found
+	# Starts to count the execution time
+	t0 = time()
+
+	# Verifies if you did a restart
+	restart_algorithm = False
+	# Count the number of restarts
+	restart_counter = 0
+	# List of solutions found
+	bins_list_solution = []
+	# Auxiliar list of solutions found
 	aux_bins_list_solution = []
-	min_num_of_bins_found = 100000 #Upper limit of bins
+	# Upper limit of number of bins
+	min_num_of_bins_found = 100000
 	
+	# While that determines the number times the first fit
+	# algorithm will be called again (Random Restart Loop)
 	while restart_counter < restart_limit:
+		# Condition used to shuffle in all iterations,
+		# except for the first one
 		if restart_algorithm:
 			shuffle(items_list)
 			restart_algorithm = False
 
+		# Calling first fit algorithm and registering
+		# the initial solution in bins_list variable
 		bins_list = first_fit_algorithm_modified(bin_capacity, items_list, False)
-		initial_best = len(bins_list)
 
+		#
 		bin_index_1 = 0
 		bin_index_2 = 0
 		bin_size_list = []
@@ -189,21 +216,29 @@ def hill_climbing_test(bin_capacity, items_list, restart_limit, total_iterations
 
 		counter = 0
 		better_counter = 0
+		# While counter doesn't reach the total iterations
+		# passed as a parameter, the algorithm will
+		# be executed (Hill Climbing Loop)
 		while (counter < total_iterations):
+			# Set better_solution boolean to false
 			better_solution = False
 
-			# Copying bins_list
+			# Copying bins_list to bins_list_aux
 			bins_list_aux = bins_list[::]
 
-			# Get total of items
+			# Get total of items of each bin
 			total_items_1 = bins_list[bin_index_1].count('1')
 			total_items_2 = bins_list[bin_index_2].count('1')
 
 			# Get both items from bin 1 and bin 2
+			# We need to convert from string to list
+			# because strings are immutable
 			item_1 = list(bins_list_aux[bin_index_1])
 			item_2 = list(bins_list_aux[bin_index_2])
 
 			# Swap items inside bin_list_aux
+			# We need to change the '0' from bin_1 by
+			# the '1' in the same position of the bin_2
 			aux = item_1[item_aux_index_1]
 			item_1[item_aux_index_1] = item_2[item_aux_index_1]
 			item_2[item_aux_index_1] = aux
@@ -212,19 +247,16 @@ def hill_climbing_test(bin_capacity, items_list, restart_limit, total_iterations
 			item_1[item_aux_index_2] = item_2[item_aux_index_2]
 			item_2[item_aux_index_2] = aux
 
+			# Transform back from list to string
 			bins_list_aux[bin_index_1] = "".join(item_1)
 			bins_list_aux[bin_index_2] = "".join(item_2)
 
-			# Getting the space left by changing the items
+			# Getting the space left after swapping the items
+			# between bins
 			size_1, aux_off = item_size(bins_list_aux[bin_index_1])
 			size_2, aux_off = item_size(bins_list_aux[bin_index_2])
 			rest_of_bin_1 = bin_capacity - size_1
 			rest_of_bin_2 = bin_capacity - size_2
-
-			# print("Bin 1:", bin_index_1)
-			# print("Bin 2:", bin_index_2)
-			# print("Item 1:", item_index_1)
-			# print("Item 2:", item_index_2)
 
 			# Verify if the change surpasses the capacity of one of the bins
 			if rest_of_bin_1 > 0 and rest_of_bin_2 > 0:
@@ -258,6 +290,11 @@ def hill_climbing_test(bin_capacity, items_list, restart_limit, total_iterations
 					bins_list_aux[bin_less_weight_index] = "".join(aux_list)
 
 					better_solution = True
+				# If the algorithm is more than "total_no_better"
+				# iterations without finding a better solution,
+				# the looping is interrupted and the algorithm
+				# and the algorithm restarts with other inital
+				# solution
 				elif better_counter > total_no_better:
 					restart_algorithm = True
 					break
@@ -278,8 +315,12 @@ def hill_climbing_test(bin_capacity, items_list, restart_limit, total_iterations
 				if len(bins_list) < len(aux_bins_list_solution):
 					aux_bins_list_solution = bins_list
 
+				# This list stores, for each bin, its total weight,
+				# the bin index in bins_list and the list of
+				# indexes of each item inside it
 				bin_size_list = []
 
+				#
 				for i, u_bin in enumerate(bins_list):
 					bin_sum, index_list = item_size(u_bin)
 					bin_size_list.append( (bin_sum, i, index_list) )
@@ -349,33 +390,33 @@ def hill_climbing_test(bin_capacity, items_list, restart_limit, total_iterations
 
 	return bins_list_solution
 
-#Function to generate output archive
-#Receives last bins' list found as solution
-#and the path to the file in which the results
-#will be written
+# Function to generate output archive
+# Receives last bins' list found as solution
+# and the path to the file in which the results
+# will be written
 def outputFile(bins_list_solution, filePath):
-	#Open file in write mode
+	# Open file in write mode
 	file = open(filePath, "w")
 
-	#Get the number of bins
+	# Get the number of bins
 	num_bins = str(len(bins_list_solution))
 
-	#Write the number of bins in file
+	# Write the number of bins in file
 	file.write(num_bins + "\n")
 
-	#For each bin in bins_list, write each item size
-	#in the same file line
+	# For each bin in bins_list, write each item size
+	# in the same file line
 	for index, one_bin in enumerate(bins_list_solution):
 		aux = ""
 		for i, string in enumerate(one_bin):
-			#Just concatenate when there's one in string
+			# Just concatenate when there's one in string
 			if string == '1':
-				#Find the items size in items_list, depending on i (index)
+				# Find the items size in items_list, depending on i (index)
 				aux += str(items_list[i]) + " "
-		#Write the items sizes in file
+		# Write the items sizes in file
 		file.write(aux + "\n")
 
-	#Close file
+	# Close file
 	file.close()
 
 ########################################
